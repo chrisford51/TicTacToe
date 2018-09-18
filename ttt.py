6023138b22666e9fa53405fe661af99eda650794
Chris Ford <cford@usicorp.usinternet.com>
2018-09-18 17:19:49 -0500

    Initial Commit, added main TicTacToe.py and test ttt.py

---
+++ ttt.py
@@ -0,0 +1,11 @@
+# ## Board Create ## 
+# def display_board(board):
+#     print(board[7]+'|'+board[8]+'|'+board[9])
+#     print(board[4]+'|'+board[5]+'|'+board[6])
+#     print(board[1]+'|'+board[2]+'|'+board[3])
+
+# ## Board Test ##
+# test_board = ['#','X','O','X','O','X','O','X','O','X']
+# display_board(test_board)
+
+
